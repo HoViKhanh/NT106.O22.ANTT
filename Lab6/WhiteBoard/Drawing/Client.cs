9101cb6882692cb29b67fec05aacc7d234684421
ViKa_618 <hovikhanh2k4@gmail.com>
2024-06-22 20:59:57 +0700

    update lab6

--- Lab6/WhiteBoard/Drawing/Client.cs
+++ Lab6/WhiteBoard/Drawing/Client.cs
@@ -1,44 +1,95 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
+using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace Drawing
 {
     public partial class Client : Form
     {
-        Graphics graphics;
-        Pen cursorPen;
+        private Graphics graphics;
+        private Pen cursorPen = new Pen(Color.Black, 2);
+        private bool drawing = false;
+        private TcpClient clientSocket = new TcpClient();
+        private NetworkStream serverStream;
+        private Thread clientThread;
 
         public Client()
         {
             InitializeComponent();
+            ConnectToServer();
+        }
+
+        private void ConnectToServer()
+        {
+            try
+            {
+                clientSocket.Connect("127.0.0.1", 100); // Địa chỉ và cổng của server
+                serverStream = clientSocket.GetStream();
+                clientThread = new Thread(GetServerResponse);
+                clientThread.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Could not connect to server: " + ex.Message);
+            }
+        }
+
+        private void GetServerResponse()
+        {
+            while (true)
+            {
+                byte[] inStream = new byte[clientSocket.ReceiveBufferSize];
+                serverStream.Read(inStream, 0, inStream.Length);
+                string returndata = Encoding.ASCII.GetString(inStream);
+                string[] tokens = returndata.Split(':');
+                if (tokens[0] == "DRAW")
+                {
+                    string[] coords = tokens[1].Split(',');
+                    int x = int.Parse(coords[0]);
+                    int y = int.Parse(coords[1]);
+                    Invoke(new Action(() =>
+                    {
+                        graphics.DrawEllipse(cursorPen, x, y, 2, 2);
+                    }));
+                }
+            }
         }
 
         private void btn_color_Click(object sender, EventArgs e)
         {
-            Color color = new Color();
-            color.Show();
+            using (ColorDialog colorDialog = new ColorDialog())
+            {
+                if (colorDialog.ShowDialog() == DialogResult.OK)
+                {
+                    cursorPen.Color = colorDialog.Color;
+                }
+            }
         }
 
         private void board_MouseDown(object sender, MouseEventArgs e)
         {
-
+            drawing = true;
         }
 
         private void board_MouseUp(object sender, MouseEventArgs e)
         {
-
+            drawing = false;
         }
 
         private void board_MouseMove(object sender, MouseEventArgs e)
         {
-
+            if (drawing)
+            {
+                graphics = board.CreateGraphics();
+                graphics.DrawEllipse(cursorPen, e.X, e.Y, 2, 2);
+                string sendData = $"DRAW:{e.X},{e.Y}";
+                byte[] outStream = Encoding.ASCII.GetBytes(sendData);
+                serverStream.Write(outStream, 0, outStream.Length);
+                serverStream.Flush();
+            }
         }
     }
 }
--- Lab6/WhiteBoard/Drawing/Server.cs
+++ Lab6/WhiteBoard/Drawing/Server.cs
@@ -1,20 +1,95 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace Drawing
 {
     public partial class Server : Form
     {
+        private Socket serverSocket;
+        private List<Socket> clientSockets = new List<Socket>();
+        private byte[] buffer = new byte[1024];
+        private int clientCount = 0;
+
         public Server()
         {
             InitializeComponent();
+            SetupServer();
+        }
+
+        private void SetupServer()
+        {
+            serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            serverSocket.Bind(new IPEndPoint(IPAddress.Any, 100)); // Lắng nghe trên cổng 100
+            serverSocket.Listen(10);
+            serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+        }
+
+        private void AcceptCallback(IAsyncResult AR)
+        {
+            Socket socket = serverSocket.EndAccept(AR);
+            clientSockets.Add(socket);
+            clientCount++;
+            UpdateClientCount();
+            socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket);
+            serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+        }
+
+        private void ReceiveCallback(IAsyncResult AR)
+        {
+            Socket current = (Socket)AR.AsyncState;
+            int received;
+            try
+            {
+                received = current.EndReceive(AR);
+            }
+            catch (SocketException)
+            {
+                // Client forcefully disconnected
+                current.Close();
+                clientSockets.Remove(current);
+                clientCount--;
+                UpdateClientCount();
+                return;
+            }
+
+            byte[] recBuf = new byte[received];
+            Array.Copy(buffer, recBuf, received);
+            string text = Encoding.ASCII.GetString(recBuf);
+
+            foreach (Socket socket in clientSockets)
+            {
+                if (socket != current)
+                {
+                    socket.Send(recBuf);
+                }
+            }
+
+            current.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), current);
+        }
+        private void UpdateClientCount()
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action(UpdateClientCount));
+                return;
+            }
+            tb_client.Text = clientCount.ToString();
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            foreach (Socket socket in clientSockets)
+            {
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            serverSocket.Close();
+            base.OnFormClosing(e);
         }
     }
 }
